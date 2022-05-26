f02ec7c35571f1bd6d811bb904be7e8ae841483c
YUZU <yuzu.twinblade@gmail.com>
2022-05-26 17:42:21 +0900

    again

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,13 @@ import { defineNuxtConfig } from 'nuxt'
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   ssr: false,
-  app: {
-    cdnURL: '/yuzu-pri-mhrise-crown-list/'
-  }
+  generate: {
+    dir: 'docs'
+  },
+  router: {
+    base: '/mhrise-crown-list/'
+  },
+  // app: {
+  //   cdnURL: '/yuzu-pri-mhrise-crown-list/'
+  // }
 })
