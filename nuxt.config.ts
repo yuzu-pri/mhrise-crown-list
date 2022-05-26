c400d2116b2d136ff879d80bd5ad22b0a757803f
YUZU <yuzu.twinblade@gmail.com>
2022-05-26 17:38:53 +0900

    fix generate paths

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,8 +2,8 @@ import { defineNuxtConfig } from 'nuxt'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  ssr: false
-  // app: {
-  //   cdnURL: '/yuzu-pri-mhrise-crown-list/'
-  // }
+  ssr: false,
+  app: {
+    cdnURL: '/yuzu-pri-mhrise-crown-list/'
+  }
 })
