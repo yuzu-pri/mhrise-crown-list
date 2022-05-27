e046ace52140363068f2b3e0afaf9fc03076b911
YUZU <yuzu.twinblade@gmail.com>
2022-05-27 09:50:22 +0900

    fix _nuxt path

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,13 +3,8 @@ import { defineNuxtConfig } from 'nuxt'
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   ssr: false,
-  // generate: {
-  //   dir: 'docs'
-  // },
-  // router: {
-  //   base: '/mhrise-crown-list/'
-  // },
   app: {
+    buildAssetsDir: '/assets/',
     baseURL: '/mhrise-crown-list/'
   }
 })
